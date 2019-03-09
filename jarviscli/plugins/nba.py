b251a8941e3f8c08a858e0bf822a8e29835bbf23
Ismail8Ahmed <warsame223@gmail.com>
2019-03-09 14:10:45 -0800

    Hello! I have only tried to specified in the requirments that the NBA scraper be included.

--- jarviscli/plugins/nba.py
+++ jarviscli/plugins/nba.py
@@ -2,10 +2,14 @@ from plugin import plugin
 from basketball_reference_web_scraper import client
 import datetime
 @plugin("nba")
-def nbascores(jarvis, s):
+def nba(jarvis, s):
     d = datetime.now()
     pyyear = int(d.strftime('%Y'))
     pymonth = int(d.strftime('%m'))
     pyday= int(d.strftime('%d'))
     s = client.team_box_scores(day=pyday, month=pymonth, year=pyyear)
-    jarvis.say(s)
+    f = str(s)
+    jarvis.say(f)
+    # if this an't working, i will probs downlaod a csv file and read from it
+
+
